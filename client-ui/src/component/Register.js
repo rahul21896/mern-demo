dbf140bf3304a298b4a89d527c4d732b7ce6cd4b
Rahul Dhamecha <65551661+rahul21896@users.noreply.github.com>
2022-11-20 03:17:32 +0530

    Login & Register Design (#1)

--- client-ui/src/component/Register.js
+++ client-ui/src/component/Register.js
@@ -1,11 +1,48 @@
 import React from "react";
+import {Button, Col, Row} from "react-bootstrap";
+import logo from "../media/logo.png";
+import {Link} from "react-router-dom";
 
 class Register extends React.Component{
     render() {
         return(
-            <>
-                <h1>Register Form</h1>
-            </>
+            <div className={`flex-div`}>
+                <div>
+                    <Row>
+                        <Col>
+                            <img src={logo} alt="Dev Rahul" className={`logo_small`}/>
+                        </Col>
+                    </Row>
+                    <div className="login_form">
+                        <Row>
+                            <Col>
+                                <h4 className={`form_title`}>Register</h4>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <input type="email" name={`email`} className={`form-control`} placeholder={`Enter Your Email`}/>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <input type="text" name={`name`} className={`form-control mt-15`} placeholder={`Enter Your Name`}/>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <input type="password" name={`password`} className={`form-control mt-15`} placeholder={`Enter Your Password`}/>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Button className={`submit_button`}>Register</Button>
+                                <Link className={`ml-10 register_link`} to={`/login`}>Already have account ? Login Here</Link>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
